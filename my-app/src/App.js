8bb937f8410842ba7bb02db205bf19a2656e57b7
Grace Yang <hanmingyang@Mac.wehi.edu.au>
2025-09-05 14:33:02 +1000

    creating a button to test

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,53 +1,30 @@
-import React, { useState, useEffect } from 'react';
-import './App.css';
-import ThreeDScene from './3d';
-
+import React from "react";
+import "./App.css";
+import ThreeDScene from "./3d";
 
 function App() {
-
-  const [timer, setTimer] = useState(0);
-  const [typedText, setTypedText] = useState('');
-
-  /* TYPEWRITTER EFFECT */
-  // useEffect(() => {
-  //   const brand = "ARES";
-  //   let i = 0;
-  //   let currentText = "";
-  
-  //   const timerId = setInterval(() => {
-  //     if (i < brand.length) {
-  //       currentText += brand[i];
-  //       setTypedText(currentText + "|");
-  //       i++;
-  //     } else {
-  //       setTypedText(currentText);
-  //       clearInterval(timerId); // Stop the interval when typing is complete
-  //     }
-  //   }, 150);
-  
-  //   return () => clearInterval(timerId); // Cleanup 
-  // }, []); // Empty dependency array to run only once when page loads
-
   return (
-     <div className="App">
+    <div className="App">
       <div id="three-container">
-        <ThreeDScene  />
+        <ThreeDScene />
       </div>
-      
 
       <header className="App-header">
-         <img className="ares-logo" src="/ares-logo.png" alt="Ares Logo"/>
+        <img className="ares-logo" src="/ares-logo.png" alt="Ares Logo" />
       </header>
- 
-      <main>
-          <div className='horizontal-container'>
-        
-              <div className='vertical-container'>
-                   
 
-              </div>
-                 
+      <main>
+        <div className="horizontal-container">
+          <div className="vertical-container">
+            {/* ✅ Test Button */}
+            <button
+              className="action-button button-start"
+              onClick={() => alert("✅ Button click works!")}
+            >
+              Test Click
+            </button>
           </div>
+        </div>
       </main>
     </div>
   );
